c4f2375f9e659774a15c3beed1286807a69690a9
ykszk <yuki.suzuki.h1@gmail.com>
2019-01-18 21:29:01 +0900

    Bump version

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pylibdeflate',
-    version='0.1.1',
+    version='0.2.0',
     author='Yuki Suzuki',
     author_email='suzuki.yuki.su6@is.naist.jp',
     description='Python interface for libdeflate',
